7a6749fbf07a5e1cbd199d1d1e7a48395fab3169
Nadia <music-hood@yandex.ru>
2022-08-08 21:57:50 +0500

    inition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+  public calcNumber!: number;
+
+  public calcForm = this.formBuilder.group({
+    age: new FormControl('', Validators.required),
+    weight: new FormControl('', Validators.required),
+    growth: new FormControl('', Validators.required),
+    sex: new FormControl('', Validators.required),
+    activity: new FormControl('', Validators.required)
+  })
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  public getActivity(): number {
+    let activityCode: number = 0;
+
+    switch (this.calcForm.controls['activity'].value) {
+      case '1': activityCode = 1.2;
+        break;
+
+      case '2': activityCode = 1.375;
+        break;
+
+      case '3': activityCode = 1.55;
+        break;
+
+      case '4': activityCode = 1.7;
+        break;
+
+      case '5': activityCode = 1.9;
+        break;
+    }
+
+    return activityCode;
+  }
+
+  public onSubmit(): void {
+    if (this.calcForm.controls['sex'].value === 'female') {
+      this.calcNumber = Math.floor((10 * this.calcForm.controls['weight'].value + 6.25 * this.calcForm.controls['growth'].value - 5 * this.calcForm.controls['age'].value - 161) * this.getActivity())
+    } else {
+      this.calcNumber = Math.floor((10 * this.calcForm.controls['weight'].value + 6.25 * this.calcForm.controls['growth'].value - 5 * this.calcForm.controls['age'].value + 5) * this.getActivity())
+    }
+  }
+
+  public resetForm():void {
+    this.calcForm.reset();
+  }
+}
